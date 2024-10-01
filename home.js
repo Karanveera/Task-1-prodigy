b859a21e1485b9d1f331657ade800ae4fc1a279a
Karanveera <122898186+Karanveera@users.noreply.github.com>
2024-10-01 21:30:44 +0530

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-      document.body.classList.add('scrolled');
-    } else {
-      document.body.classList.remove('scrolled');
-    }
-  });
-  
